531400270cdc3ecdb158a888aaa9dcc2cd671c6d
fernandoN10 <152297932+fernandoN10@users.noreply.github.com>
2024-01-31 10:05:30 +0100

    Add files via upload

--- aplication.py
+++ aplication.py
@@ -17,7 +17,7 @@ st.write("")
 
 with st.sidebar:
     with st.form("filters"):
-        film_name = st.text_input("Enter the name of your film")
+        film_name = st.text_input("Enter the name of your film").lower()
         years = st.slider("Release date", dataset_ML["startYear"].min(), dataset_ML["startYear"].max(), (dataset_ML["startYear"].min(),     dataset_ML["startYear"].max()))
 
         submitted = st.form_submit_button("Submit")
@@ -26,36 +26,60 @@ with st.sidebar:
             X = dataset_ML.select_dtypes(include='number')
             y = dataset_ML['primaryTitle']
             
-            #Standardisation des variables explicatives et formation du modèle
-            X_scaled = StandardScaler().fit_transform(X)
-            model = KNeighborsClassifier(n_neighbors=10, weights='distance').fit(X_scaled, y)
+            #Standardisation des variables explicatives 
+            X_scaled = StandardScaler().fit_transform(X)       
+
+            #Calculo de indices en donde se encuentran las columnas de actores, directores y generos
+            genres1 = ['Action', 'Adventure', 'Animation', 'Sci-Fi']
+            genres2 = ['Comedy', 'Crime', 'Drama', 'Family', 'Film-Noir', 'Horror', 'Romance']
+            posiciones_genres1 = [X.columns.get_loc(col) for col in genres1]
+            posiciones_genres2 = [X.columns.get_loc(col) for col in genres2]
+            posiciones_actor = [X.columns.get_loc(col) for col in X.columns if col.startswith('actor')]
+            posiciones_actress = [X.columns.get_loc(col) for col in X.columns if col.startswith('actress')]
+            posiciones_directors = [X.columns.get_loc(col) for col in X.columns if col.startswith('director')]
+            posiciones_actors = posiciones_actor + posiciones_actress
+
+            X_scaled[:, posiciones_genres1] *= 20
+            X_scaled[:, posiciones_genres2] *= 2
+            X_scaled[:, posiciones_actors] *= 20
+            X_scaled[:, posiciones_directors] *= 30
+
+            #formation du modèle
+            model = KNeighborsClassifier(n_neighbors=25, weights='distance').fit(X_scaled, y)
             
             #Recherche de l'indice du film recherché et des films recommandès
-            
-            index_movie = dataset_ML.loc[dataset_ML['primaryTitle'] == film_name].index[0]
-            recom = model.kneighbors([X_scaled[index_movie]])[1][0]
-            dataset_recom = dataset_ML.iloc[recom]
-    
-            dataset_recom = dataset_recom.loc[dataset_recom.startYear.between(years[0], years[1])]
+            dataset_ML['primaryTitle_low'] = dataset_ML['primaryTitle'].apply(lambda x: x.lower())
+            #Voy a modifica la siguiente linea para que al realizar la busqueda no importe si se escribe mal
+            #idx = dataset_ML.loc[dataset_ML['primaryTitle_low'] == film_name].index
+            idx = dataset_ML.loc[dataset_ML['primaryTitle_low'].str.contains(film_name)].index
+            if not idx.empty:
+                index_movie = idx[0]
+                recom = model.kneighbors([X_scaled[index_movie]])[1][0]
+                dataset_recom = dataset_ML.iloc[recom]
+                dataset_recom = dataset_recom.loc[dataset_recom.startYear.between(years[0], years[1])]
+            else:
+                st.write("Vérifie le nom du film ou essaie avec un autre")
+                dataset_recom = dataset_ML.loc[dataset_ML.startYear.between(years[0], years[1])]
         else:
             dataset_recom = dataset_ML.loc[dataset_ML.startYear.between(years[0], years[1])]
 
-row1, row2, row3, row4 = st.columns(3), st.columns(3), st.columns(3), st.columns(3)
-            
-for i, col in zip(range(len(dataset_recom)), row1 + row2 + row3 + row4):
-    with col.container():
-        col1, col2 = st.columns(2)
-                    
-        with col1:
-            st.image(url + dataset_recom.iloc[i]['poster_path'], use_column_width="auto")
-                    
-        with col2:
-            st.header(dataset_recom.iloc[i]['primaryTitle'])
-            st.write("Actors: " + str(dataset_recom.iloc[i]['liste_actors']))
-            st.write("Realease Date: " + str(dataset_recom.iloc[i]['startYear']))
-            st.write("Runtime: " + str(dataset_recom.iloc[i]['runtimeMinutes']) + "min")
-
-# In[ ]:
+if not dataset_recom.empty:
+    row1, row2, row3, row4, row5 = st.columns(3), st.columns(3), st.columns(3), st.columns(3), st.columns(3)
+                
+    for i, col in zip(range(len(dataset_recom)), row1 + row2 + row3 + row4 + row5):
+        with col.container():
+            col1, col2 = st.columns(2)
+                        
+            with col1:
+                st.image(url + dataset_recom.iloc[i]['poster_path'], use_column_width="auto")
+                        
+            with col2:
+                st.header(dataset_recom.iloc[i]['primaryTitle'])
+                st.write("Actors: " + str(dataset_recom.iloc[i]['liste_actors']))
+                st.write("Realease Date: " + str(dataset_recom.iloc[i]['startYear']))
+                st.write("Runtime: " + str(dataset_recom.iloc[i]['runtimeMinutes']) + "min")
+else:
+    dataset_recom = dataset_ML
 
 
 
